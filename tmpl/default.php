49e6897ab4ab0f7eca393434b8138a87458c7393
Jochen Stehle <josh@stingray21.fake>
2014-11-05 20:13:54 -0800

    small changes on DB column names and display

--- tmpl/default.php
+++ tmpl/default.php
@@ -19,7 +19,7 @@ if ($posLeague == 'above') {
 echo "<table class=\"HBstandings HBhighlight\">";
 echo "<thead>";
 echo "<tr><th>Platz</th><th class=\"textteam\">Mannschaft</th>"
-	. "<th>Sp.</th><th>S</th><th>U</th><th>N</th><th colspan=\"3\">Tore</th>"
+	. "<th>Sp.</th><th>S</th><th>U</th><th>N</th><th colspan=\"3\" class=\"goals\">Tore</th>"
 	. "<th>Diff.</th><th colspan=\"3\">Punkte</th></tr>";
 echo "</thead>\n";
 
@@ -30,10 +30,10 @@ echo "<tbody>";
 		if ($row->heimVerein) echo ' heim';
 		echo "\">";
 		echo "<td>{$row->platz}</td><td class=\"textteam\"><strong>{$row->mannschaft}</strong></td>";
-		echo "<td>{$row->spiele}</td><td>{$row->siege}</td><td>{$row->unentschieden}</td><td>{$row->niederlagen}</td>";
-		echo "<td>{$row->tore}</td><td>:</td><td>{$row->gegenTore}</td>";
-		echo "<td>{$row->torDifferenz}</td>";
-		echo "<td><strong>{$row->punkte}</strong></td><td>:</td><td><strong>{$row->minusPunkte}</strong></td></tr>\n";
+		echo "<td>{$row->spiele}</td><td>{$row->s}</td><td>{$row->u}</td><td>{$row->n}</td>";
+		echo "<td class=\"goals\">{$row->tore}</td><td class=\"sepaDots\">:</td><td class=\"goalsCon\">{$row->gegenTore}</td>";
+		echo "<td>{$row->torDiff}</td>";
+		echo "<td class=\"points\"><strong>{$row->punkte}</strong></td><td class=\"sepaDots\">:</td><td class=\"negPoints\"><strong>{$row->minusPunkte}</strong></td></tr>\n";
 	}
 echo "</tbody>";
 echo "</table>\n";
@@ -42,7 +42,7 @@ echo "</table>\n";
 echo "<table class=\"HBstandings HBhighlight\">";
 echo "<thead>";
 echo "<tr><th>Platz</th><th class=\"textteam\">Mannschaft</th>"
-	. "<th>Sp.</th><th>S-U-N</th><th>S-U-N(H)</th><th>S-U-N(A)</th><th colspan=\"3\">Tore</th>"
+	. "<th>Sp.</th><th>S-U-N</th><th>S-U-N(H)</th><th>S-U-N(A)</th><th colspan=\"3\" class=\"goals\">Tore</th>"
 	. "<th>Diff.</th><th colspan=\"3\">Punkte</th></tr>";
 echo "</thead>\n";
 
@@ -54,12 +54,12 @@ echo "<tbody>";
 		echo "\">";
 		echo "<td>{$row->platz}</td><td class=\"textteam\"><strong>{$row->mannschaft}</strong></td>";
 		echo "<td>{$row->spiele}</td>";
-		echo "<td>{$row->siege}-{$row->unentschieden}-{$row->niederlagen}</td>";
-		echo "<td>{$row->siegeH}-{$row->unentschiedenH}-{$row->niederlagenH}</td>";
-		echo "<td>{$row->siegeA}-{$row->unentschiedenA}-{$row->niederlagenA}</td>";
-		echo "<td>{$row->tore}</td><td>:</td><td>{$row->gegenTore}</td>";
-		echo "<td>{$row->torDifferenz}</td>";
-		echo "<td><strong>{$row->punkte}</strong></td><td>:</td><td><strong>{$row->minusPunkte}</strong></td></tr>\n";
+		echo "<td>{$row->s}-{$row->u}-{$row->n}</td>";
+		echo "<td>{$row->sH}-{$row->uH}-{$row->nH}</td>";
+		echo "<td>{$row->sA}-{$row->uA}-{$row->nA}</td>";
+		echo "<td class=\"goals\">{$row->tore}</td><td class=\"sepaDots\">:</td><td class=\"goalsCon\">{$row->gegenTore}</td>";
+		echo "<td>{$row->torDiff}</td>";
+		echo "<td class=\"points\"><strong>{$row->punkte}</strong></td><td class=\"sepaDots\">:</td><td class=\"negPoints\"><strong>{$row->minusPunkte}</strong></td></tr>\n";
 	}
 echo "</tbody>";
 echo "</table>\n";
